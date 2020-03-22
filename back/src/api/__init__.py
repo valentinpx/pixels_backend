52c4663d68111ad685a9e8a3611370229d3233ae
Valentin Sene <valentin.sene@epitech.eu>
2020-03-22 12:40:17 +0100

    * pixels.db all pixels white * __init__.py * main.c launches api route to get all (or one) pixel(s) and route to modify a pixel (unsecured)

---
+++ back/src/api/__init__.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from random import randint
+
+# Creating intance of Flask class: the application
+app = Flask(__name__)
+
+# Allowing api access from any server
+CORS(app)
+
+# Configuring database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../../db/pixels.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Defining the db main table with a class
+class Pixel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color = db.Column(db.String(6))
+    key = db.Column(db.Integer)
+
+    def __init__(self, color):
+        self.color = color
+        self.key = randint(0, 100000)
+
+# Defining what URL should trigger a function
+@app.route("/")
+def hello_world():
+    return ("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Click for a surprise</a>")
+
+@app.route("/pixels")
+def list_all():
+    pixels = Pixel.query.all()
+    dest = []
+
+    for pixel in pixels:
+        dest.insert(0, {"id": pixel.id, "color": pixel.color})
+    return (jsonify(dest))
+
+@app.route("/pixels/<id>")
+def get_specified(id):
+    pixel = Pixel.query.get(id)
+
+    if (pixel != None):
+        return ({"id": id, "color": pixel.color})
+    else:
+        return ("Not found", 404)
+
+@app.route("/pixels/<id>/edit", methods = ['POST'])
+def edit(id):
+    pixel = Pixel.query.get(id)
+    color = request.args.get('color')
+
+    if (pixel != None):
+        pixel.color = color
+        db.session.commit()
+        return ({"id": pixel.id, "color": pixel.color})
+    return ("Not found", 404)
+
---
+++ back/src/main.py
@@ -0,0 +1,5 @@
+from api import app
+
+
+# Launching the application
+app.run(host='0.0.0.0', port=80, debug=True)
